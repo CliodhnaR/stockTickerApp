b00833a4192d0280213a159d117f91a7dcc92e0d
CliodhnaR <113465549+CliodhnaR@users.noreply.github.com>
2022-12-01 14:26:42 -0500

    Update tickerWebApp.js

--- tickerWebApp.js
+++ tickerWebApp.js
@@ -1,40 +1,42 @@
 const MongoClient = require('mongodb').MongoClient;
 const connStr = 'mongodb+srv://creidy02:Danny0512!@cluster0.2ygoinn.mongodb.net/myFirstDatabase';
 
-const userInput = $("input[name='userInput']").val();
+client = new MongoClient(url,{ useUnifiedTopology: true });
 
-$("input[type='button']").click(findTicker());
-
-client =new MongoClient(url,{ useUnifiedTopology: true });
-async function findTicker() {
+$("input[type='button']").click(async function() {
     try {
+        // connect to client + open collection
         await client.connect();
         var dbo = client.db('equities');
         var collection = dbo.collection('equitiesFinal');
-
+        // get input
+        const userInput = $("input[name='userInput']").val();
+        // get radio button choice
         let choice = $("input[name='s_or_c']:checked").val();
+        // based on choice, choose an output
         if(choice == "symbol"){
-            let search = "ticker: userInput";
+            let search = "ticker: " + userInput;
         }
         else {
-            let search = "company: userInput";
+            let search = "company: " + userInput;
         }
 
+        // search for specified result
         const curs = coll.find({},search);
 
         // print a message if no documents were found
         if ((await curs.count()) === 0) {
-            console.log("No documents found!");
+            $("#result").html("No matches found! Try again.");
         }
-        //await curs.forEach(console.dir);
+        //print each item that matches;
         await curs.forEach(function(item){
-            console.log(item.title);
+            $("#result").html("Company Name: " + item.company + "</br> Symbol: " + item.ticker);
         });
-    } 
+    }
     catch(err) {
         console.log("Database error: " + err);
     }
     finally {
         client.close();
     }
-}
+});
